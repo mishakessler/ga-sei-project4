9c15bffb93a310c5a578f7ae54b10bfcb419cc47
Misha Kessler <misha.kessler@gmail.com>
2019-08-14 09:02:32 -0400

    Add functional and class components, service files, and pages.

--- crisis_international/client/src/App.js
+++ crisis_international/client/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ crisis_international/client/src/class/IndexListings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IndexListings extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
